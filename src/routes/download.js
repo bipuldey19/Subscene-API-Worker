c7051d1d77c6b1bf591e8c16e13080e30da38d3d
Bipul Dey <bipuldey19@gmail.com>
2023-09-14 18:40:29 +0600

    Update download.js

--- src/routes/download.js
+++ src/routes/download.js
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import unzip from "adm-zip";
 import apiDownload from "../helpers/apiRequestRawHtml";
 
 const download = new Hono();
@@ -15,23 +14,7 @@ download.get("/subtitles/:lan/:id/:name", async (c) => {
 
     const downloadUrl = `${baseUrl}/subtitles/${lan}/${id}`;
 
-    const file = await apiDownload(downloadUrl);
-
-    if (!file || file.length === 0) {
-      throw new Error("File Downloading Error");
-    }
-
-    let zip = new unzip(file);
-    let zipData = [];
-    zip.getEntries().map((e, i) => {
-      if (e.name && e.getData && e.getData())
-        zipData.push({
-          filename: e.name,
-          file: e.getData(),
-        });
-    });
-
-    return c.body(zipData);
+    return c.body("Hello there!!!")
   } catch (err) {
     console.error(err);
     c.status(500).json("Error while fetching data");
